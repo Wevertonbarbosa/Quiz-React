64ae860c7448c5419036e19ad7125076f36b0506
wevertonbarbosa <wevertonlbs00@gmail.com>
2022-10-07 03:18:51 -0300

    criando o setUp

---
+++ quiz/src/components/welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const welcome = () => {
+    return (
+        <div>
+            <h2>Seja Bem-Vindo</h2>
+            <p>Clique no botão abaixo para começar:</p>
+            <button>Iniciar</button>
+        </div>
+    );
+};
+
+export default welcome;
